b9c90c995c90e3b1478c1ff63f2f90c5ff1a2d3f
Aleena Ali Azeem <99934126+aleenaazeem@users.noreply.github.com>
2022-10-16 20:23:26 +0500

    aleena's commit

--- Searching Algos/Binary_Search.py
+++ Searching Algos/Binary_Search.py
@@ -1,29 +1,28 @@
 # Binary Search in python
-
-def binarySearch(array, x, low, high):
-
-    while low <= high:
-
-        mid = low + (high - low)//2
-
-        if array[mid] == x:
-            return mid
-
-        elif array[mid] < x:
-            low = mid + 1
-
-        else:
-            high = mid - 1
-
-    return -1
+class binarySearch:
+    def __init__(self):
+        self.a = 0
+    def binarySearch(self,array, x, low, high):
+        while low <= high:
+            mid = low + (high - low)//2
+            if array[mid] == x:
+                return mid
+            elif array[mid] < x:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
 
 
-array = [3, 4, 5, 6, 7, 8, 9]
-x = int(input("Enter a number to search : "))
+def main():
 
-result = binarySearch(array, x, 0, len(array)-1)
+    array = [3, 4, 5, 6, 7, 8, 9]
+    x = int(input("Enter a number to search : "))
+    b = binarySearch()
+    result = b.binarySearch(array, x, 0, len(array)-1)
+    if result != -1:
+        print("Element is present at index " + str(result))
+    else:
+        print("Not found")
+main()
 
-if result != -1:
-    print("Element is present at index " + str(result))
-else:
-    print("Not found")
